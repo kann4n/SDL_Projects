137ef881165f15b99e41116eb6f523b2ef30a3d9
kann4n <kannan.codes456@gmail.com>
2026-04-19 17:51:14 +0530

    refactor 3141

--- 3141/3141.c
+++ 3141/3141.c
@@ -1,10 +1,9 @@
 #include <SDL3/SDL.h>
 
-
 #define SCRN_W 1200
 #define SCRN_H 900
 #define DENSITY 100
-#define BOX_Y (SCRN_H*0.75)
+#define BOX_Y (SCRN_H * 0.75)
 #define WALL_THICK 5
 
 SDL_Window *window;
@@ -15,10 +14,10 @@ struct Box
 	double x, vx, side_len, mass;
 };
 
-struct Box small_box = {SCRN_W*0.25, 0, 30, 1};
-struct Box big_box = {SCRN_W*0.50, -2, 60, 10000};
-SDL_FRect sidewall = {SCRN_W*0.1, 0, WALL_THICK, SCRN_H*0.76};
-SDL_FRect ground = {SCRN_W*0.1, SCRN_H*0.751, SCRN_W, WALL_THICK};
+struct Box small_box = {SCRN_W * 0.25, 0, 30, 1};
+struct Box big_box = {SCRN_W * 0.50, -2, 60, 10000};
+SDL_FRect sidewall = {SCRN_W * 0.1, 0, WALL_THICK, SCRN_H * 0.76};
+SDL_FRect ground = {SCRN_W * 0.1, SCRN_H * 0.751, SCRN_W, WALL_THICK};
 unsigned int counter = 0;
 
 void draw_box(struct Box);
@@ -28,13 +27,13 @@ int main()
 {
 	SDL_Init(SDL_INIT_VIDEO);
 
-	if(!SDL_CreateWindowAndRenderer("3141", SCRN_W, SCRN_H, 0, &window, &renderer))
+	if (!SDL_CreateWindowAndRenderer("3141", SCRN_W, SCRN_H, 0, &window, &renderer))
 		return -1;
 
 	SDL_SetRenderVSync(renderer, 1);
 
 	int running = 1;
-	while(running)
+	while (running)
 	{
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
@@ -47,9 +46,9 @@ int main()
 		SDL_RenderClear(renderer);
 		// update
 		double steps = 100000; // incress for higher mass | fine grading
-		for (double i=0; i<steps; i++)
+		for (double i = 0; i < steps; i++)
 			update_boxes(steps);
-		
+
 		// draw wall
 		SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
 		SDL_RenderFillRect(renderer, &sidewall);
@@ -58,14 +57,14 @@ int main()
 		// draw
 		draw_box(small_box);
 		draw_box(big_box);
-		
+
 		// draw counter
 		char buffer[128];
 		int scale = 4;
-		SDL_snprintf(buffer, sizeof(buffer), "%u", counter);	
+		SDL_snprintf(buffer, sizeof(buffer), "%u", counter);
 		SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
 		SDL_SetRenderScale(renderer, scale, scale);
-		SDL_RenderDebugText(renderer, (sidewall.x + 100)/scale, (100)/scale, buffer);
+		SDL_RenderDebugText(renderer, (sidewall.x + 100) / scale, (100) / scale, buffer);
 		SDL_SetRenderScale(renderer, 1, 1);
 		// update screen
 		SDL_RenderPresent(renderer);
@@ -86,8 +85,8 @@ void draw_box(struct Box box)
 
 void update_boxes(double steper)
 {
-	small_box.x += small_box.vx/steper;
-	big_box.x += big_box.vx/steper;
+	small_box.x += small_box.vx / steper;
+	big_box.x += big_box.vx / steper;
 	if (small_box.x < sidewall.x)
 	{
 		small_box.x = sidewall.x;
@@ -101,12 +100,12 @@ void update_boxes(double steper)
 		double v1 = small_box.vx;
 		double m2 = big_box.mass;
 		double v2 = big_box.vx;
-		
+
 		double overlap = small_box.x + small_box.side_len - big_box.x;
-		small_box.x -= overlap/2;
-		big_box.x += overlap/2;
-		small_box.vx = ((m1-m2)*v1 + 2*m2*v2) / total_mass;
-		big_box.vx = ((m2-m1)*v2 + 2*m1*v1) / total_mass;
+		small_box.x -= overlap / 2;
+		big_box.x += overlap / 2;
+		small_box.vx = ((m1 - m2) * v1 + 2 * m2 * v2) / total_mass;
+		big_box.vx = ((m2 - m1) * v2 + 2 * m1 * v1) / total_mass;
 		counter++;
 	}
 }
